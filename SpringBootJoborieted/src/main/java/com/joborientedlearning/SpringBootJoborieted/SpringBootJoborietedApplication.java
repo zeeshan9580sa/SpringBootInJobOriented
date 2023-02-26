1677fd2a6ed7fa2400d4660c9f414533f81e7300
Zeeshan Ali <zeeshanali9580@gmail.com>
2023-02-26 22:01:10 +0530

    First commit , In this version just created the project and added the Syout in main class

---
+++ SpringBootJoborieted/src/main/java/com/joborientedlearning/SpringBootJoborieted/SpringBootJoborietedApplication.java
@@ -0,0 +1,14 @@
+package com.joborientedlearning.SpringBootJoborieted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJoborietedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJoborietedApplication.class, args);
+		System.out.println("Spring Boot Application has started ");
+	}
+
+}
---
+++ SpringBootJoborieted/src/test/java/com/joborientedlearning/SpringBootJoborieted/SpringBootJoborietedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joborientedlearning.SpringBootJoborieted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJoborietedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
